0addfe99d8f72b5670837f7541831c56910133de
Alex Xu <ayx2@case.edu>
2019-03-11 23:10:08 -0700

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Inverse Logistic Regression Recommender
+Created on 2019
+
+@author: Alex Xu <ayx2@case.edu>
+"""
+from .predict_feature_values import inverse_logistic_regression_recommender
+
+__all__ = [
+    'inverse_logistic_regression_recommender'
+]
+
